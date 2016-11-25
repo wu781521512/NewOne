e285475f0bd69b7f70778b7b661173b456c76eb0
Mr.wuchao <781521512@qq.com>
2016-11-25 14:56:22 +0800

    添加三方的分享功能

--- ZhangTu/src/main/java/com/example/mrwuchao/newone/activity/MostActivity.java
+++ ZhangTu/src/main/java/com/example/mrwuchao/newone/activity/MostActivity.java
@@ -43,6 +43,8 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 
 public class MostActivity extends NotitleActivity {
 
@@ -137,6 +139,7 @@ public class MostActivity extends NotitleActivity {
                 finish();
                 break;
             case R.id.most_share:
+                showShare();
                 break;
             case R.id.most_map:
                 break;
@@ -152,6 +155,33 @@ public class MostActivity extends NotitleActivity {
                 shareTip();
                 break;
         }
+    }
+
+    private void showShare() {
+
+            ShareSDK.initSDK(this);
+            OnekeyShare oks = new OnekeyShare();
+            //关闭sso授权
+            oks.disableSSOWhenAuthorize();
+            // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间等使用
+            oks.setTitle(mostInfo.getShare_message());
+            // titleUrl是标题的网络链接，QQ和QQ空间等使用
+            //oks.setTitleUrl("http://sharesdk.cn");
+            // text是分享文本，所有平台都需要这个字段
+            oks.setText(mostInfo.getShare_message());
+            // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+            //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+            // url仅在微信（包括好友和朋友圈）中使用
+            oks.setUrl(mostInfo.getSub_share_url());
+            // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+            //oks.setComment("我是测试评论文本");
+            // site是分享此内容的网站名称，仅在QQ空间使用
+            //oks.setSite(getString(R.string.app_name));
+            // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+            oks.setSiteUrl(mostInfo.getSub_share_url());
+
+            // 启动分享GUI
+            oks.show(this);
     }
 
     public void toPin(){
--- ZhangTu/src/main/java/com/example/mrwuchao/newone/adapter/MostAdapter.java
+++ ZhangTu/src/main/java/com/example/mrwuchao/newone/adapter/MostAdapter.java
@@ -23,6 +23,9 @@ import com.example.mrwuchao.newone.views.MyLinearManager;
 
 import java.util.List;
 
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
+
 /**
  * 发现页面的分类界面的适配器
  */
@@ -42,7 +45,7 @@ public class MostAdapter extends RecyclerView.Adapter<MostAdapter.MyHolder> {
 
     @Override
     public void onBindViewHolder(MyHolder holder, int position) {
-        MostDataInfo mostDataInfo = mostInfoList.get(position);
+        final MostDataInfo mostDataInfo = mostInfoList.get(position);
         Glide.with(context).load(mostDataInfo.getData_img()).placeholder(R.mipmap.icon_loading_end).into(holder.imageView);
         holder.imageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -91,6 +94,28 @@ public class MostAdapter extends RecyclerView.Adapter<MostAdapter.MyHolder> {
             @Override
             public void onClick(View v) {
                 //添加分享功能
+                    ShareSDK.initSDK(context);
+                    OnekeyShare oks = new OnekeyShare();
+                    //关闭sso授权
+                    oks.disableSSOWhenAuthorize();
+                    // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间等使用
+                    oks.setTitle(mostDataInfo.getShare_message());
+                    // titleUrl是标题的网络链接，QQ和QQ空间等使用
+                    //oks.setTitleUrl("http://sharesdk.cn");
+                    // text是分享文本，所有平台都需要这个字段
+                    oks.setText(mostDataInfo.getShare_message());
+                    // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+                    //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+                    // url仅在微信（包括好友和朋友圈）中使用
+                    oks.setUrl(mostDataInfo.getData_share_url());
+                    // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+                    //oks.setComment("我是测试评论文本");
+                    // site是分享此内容的网站名称，仅在QQ空间使用
+                    //oks.setSite(getString(R.string.app_name));
+                    // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+                    oks.setSiteUrl(mostDataInfo.getData_share_url());
+                    // 启动分享GUI
+                    oks.show(context);
             }
         });
     }
